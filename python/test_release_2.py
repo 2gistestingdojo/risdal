346ad377e7de5abdcdf3c55484f06f01f0d11378
risdal <risdal@fake.com>
2015-06-06 12:16:44 +0600

    3 test

--- python/test_release_2.py
+++ python/test_release_2.py
@@ -48,3 +48,17 @@ class Release2Test(BaseTestCase):
         items_number_after = len(product_items_after)
 
         self.assertEqual(items_number_after, items_number_before)
+
+    def test_search_by_id(self):
+        main_window = self.driver.find_element_by_id('MainWindow')
+
+        search_string = main_window.find_element_by_id('QueryMW')
+        search_string.send_keys('1')
+
+        search_button = main_window.find_element_by_id('SearchMW')
+        search_button.click()
+
+        products_list = main_window.find_element_by_id('ProductsMW')
+        product_items = products_list.find_elements_by_class_name('ListViewItem')
+
+        self.assertEqual(len(product_items), 2)
