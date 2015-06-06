d7739308bde9b499ca1c439f1d2fc1c77e5da955
risdal <risdal@fake.com>
2015-06-06 14:29:09 +0600

    new file

---
+++ python/test_release_6.py
@@ -0,0 +1,8 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from base_test_case import BaseTestCase
+import unittest
+
+class Release6Test(BaseTestCase):
+    def test_save_edition(self):
+        pass
