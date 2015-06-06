bd066abb4ad33647ab6ff7f6a96ebca29035b97d
risdal <risdal@fake.com>
2015-06-06 12:29:33 +0600

    blah

--- python/test_release_2.py
+++ python/test_release_2.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.keys import Keys
 from base_test_case import BaseTestCase
 import unittest
 
@@ -49,7 +50,7 @@ class Release2Test(BaseTestCase):
 
         self.assertEqual(items_number_after, items_number_before)
 
-    def test_search_by_id(self):
+    def test_search_by_id_with_button(self):
         main_window = self.driver.find_element_by_id('MainWindow')
 
         search_string = main_window.find_element_by_id('QueryMW')
@@ -57,6 +58,17 @@ class Release2Test(BaseTestCase):
 
         search_button = main_window.find_element_by_id('SearchMW')
         search_button.click()
+
+        products_list = main_window.find_element_by_id('ProductsMW')
+        product_items = products_list.find_elements_by_class_name('ListViewItem')
+
+        self.assertEqual(len(product_items), 2)
+
+    def test_search_by_id_with_enter(self):
+        main_window = self.driver.find_element_by_id('MainWindow')
+
+        search_string = main_window.find_element_by_id('QueryMW')
+        search_string.send_keys('1', Keys.ENTER)
 
         products_list = main_window.find_element_by_id('ProductsMW')
         product_items = products_list.find_elements_by_class_name('ListViewItem')
