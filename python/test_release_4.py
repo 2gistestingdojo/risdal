1ef0a8999944d3014b77bbc5b6271ee2c73a7460
risdal <risdal@fake.com>
2015-06-06 13:43:01 +0600

    2

--- python/test_release_4.py
+++ python/test_release_4.py
@@ -20,3 +20,28 @@ class Release4Test(BaseTestCase):
 
         self.assertNotEqual(items_before, items_after)
 
+    def test_mystic_search(self):
+        main_window = self.driver.find_element_by_id('MainWindow')
+
+        search_button = main_window.find_element_by_id('AddNewProductMW')
+        search_button.click()
+
+        add_new_product_window = self.driver.find_element_by_id('AddNewProductWindow')
+        search_string = add_new_product_window.find_element_by_id('NameAW')
+        search_string.send_keys('9')
+
+        add_button = add_new_product_window.find_element_by_id('AddAW')
+        add_button.click()
+
+        search_string = main_window.find_element_by_id('QueryMW')
+        search_string.send_keys('9')
+
+        search_button = main_window.find_element_by_id('SearchMW')
+        search_button.click()
+
+        products_list = main_window.find_element_by_id('ProductsMW')
+        product_items = products_list.find_elements_by_class_name('ListViewItem')
+        items_number = len(product_items)
+
+        # mystic bug
+        self.assertEqual(items_number, 1)
